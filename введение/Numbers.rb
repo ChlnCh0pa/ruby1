caca98aef0bd464db30e9afc37c8751ce4e30660
Sergey Dudo <zamirdruazbu@gmail.com>
2024-10-05 12:28:51 +0300

    нахождения произведения  делителей числа

--- введение/Numbers.rb
+++ введение/Numbers.rb
@@ -18,34 +18,34 @@ def prime?(num)
   true
 end 
 
-# Метод для нахождения количества нечетных цифр числа, больших 3
-def odd_digits_greater_than_3_count(n)
-  count = 0
-  n_str = n.to_s
-  for i in 0...n_str.length
-    digit = n_str[i].to_i
-    if digit.odd? && digit > 3
-      count += 1
-    end
-  end
-  count
-end
-
-# # Метод для нахождения произведения таких делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа
-# def product_of_special_divisors(n)
-  # sum_digits_of_n = sum_of_digits(n)
-  # product = 1
-  # has_divisors = false
-
-  # for i in 1..n
-    # if n % i == 0 && sum_of_digits(i) < sum_digits_of_n
-      # product *= i
-      # has_divisors = true
+# # Метод для нахождения количества нечетных цифр числа, больших 3
+# def odd_digits_greater_than_3_count(n)
+  # count = 0
+  # n_str = n.to_s
+  # for i in 0...n_str.length
+    # digit = n_str[i].to_i
+    # if digit.odd? && digit > 3
+      # count += 1
     # end
   # end
-
-  # has_divisors ? product : 0
+  # count
 # end
+
+# Метод для нахождения произведения таких делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа
+def product_of_special_divisors(n)
+  sum_digits_of_n = sum_of_digits(n)
+  product = 1
+  has_divisors = false
+
+  for i in 1..n
+    if n % i == 0 && sum_of_digits(i) < sum_digits_of_n
+      product *= i
+      has_divisors = true
+    end
+  end
+
+  has_divisors ? product : 0
+end
 
 # # Метод для нахождения суммы цифр числа
 # def sum_of_digits(num)
