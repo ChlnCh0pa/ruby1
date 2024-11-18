f459f70ab892badd1f6431635370420a283c8b82
Sergey Dudo <zamirdruazbu@gmail.com>
2024-11-18 22:23:09 +0300

    Lab2_Task1.1. Added setters and getters  and fields to Student class

--- student/student.rb
+++ student/student.rb
@@ -1,3 +1,14 @@
 class Student
+attr_accessor :name, :surname, :patronymic, :emai:, :git, :telegram, :id, :phone
+def initialize(name:, surname:, patronymic:, email:, git:, telegram: nil, id: nil, phone: nil) 
 
+@name = name
+@surname = surname
+@patronymic = patronymic
+@emai = email
+@git = git
+@telegram = telegram
+@id = id
+@phone = phone
+end
 end
