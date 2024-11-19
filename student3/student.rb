cf8b27794a2794301912d9eadfc1b36d4ca33eb9
Sergey Dudo <zamirdruazbu@gmail.com>
2024-11-19 17:58:52 +0300

    Lab2_Task1.1.Added a constructor and implemented getter and setter methods for each field.

--- student3/student.rb
+++ student3/student.rb
@@ -1,3 +1,83 @@
 class Student
+def initialize (name:, surname:, patronymic:, git:, email:, id: nil, phone: nil, telegram: nil)
+@name = name
+@surname = surname
+@patronymic = patronymic
+@git = git
+@email = email
+@id = id 
+@phone = phone 
+@telegram = telegram
+end
 
+
+
+ def name 
+@name
+end 
+
+def name=(value)
+@name = value
+end
+
+def surname
+@surname
+end
+
+def surname=(value)
+@surname = value
+end
+
+def patronymic
+@patronymic
+end
+
+def patronymic=(value)
+@patronymic = value
+end
+
+def git
+@git
+end
+
+def git=(value)
+@git = value
+end
+
+
+def email
+@email
+end
+
+def email=(value)
+@email = value
+end
+
+def phone
+@phone
+end
+
+def phone=(value)
+@phone = value
+end
+
+def telegram
+@telegram
+end
+
+def telegram=(value)
+@telegram = value
+end
+
+def id
+@id
+end
+
+def id=(value)
+@id = value
+end
+
+
+
+ 
 end
