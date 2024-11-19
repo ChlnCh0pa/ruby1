12469039aa1dd79c4d1a2b0c9ffdbd8350264037
Sergey Dudo <zamirdruazbu@gmail.com>
2024-11-19 18:39:34 +0300

    Lab2_Task1.4. Refactor code to use attr_accessor for automatic generation of getters and setters

--- student3/student.rb
+++ student3/student.rb
@@ -1,4 +1,5 @@
 class Student
+attr_accessor :name, :surname, :patronymic, :git, :email, :id, :phone, :tlegram
 def initialize (name:, surname:, patronymic:, git:, email:, id: nil, phone: nil, telegram: nil)
 @name = name
 @surname = surname
@@ -12,70 +13,7 @@ end
 
 
 
- def name 
-@name
-end 
-
-def name=(value)
-@name = value
-end
-
-def surname
-@surname
-end
-
-def surname=(value)
-@surname = value
-end
-
-def patronymic
-@patronymic
-end
-
-def patronymic=(value)
-@patronymic = value
-end
-
-def git
-@git
-end
-
-def git=(value)
-@git = value
-end
-
-
-def email
-@email
-end
-
-def email=(value)
-@email = value
-end
-
-def phone
-@phone
-end
-
-def phone=(value)
-@phone = value
-end
-
-def telegram
-@telegram
-end
-
-def telegram=(value)
-@telegram = value
-end
-
-def id
-@id
-end
-
-def id=(value)
-@id = value
-end
+ 
 
 def to_s
 "Студент\nID: #{@id}\nИмя: #{@name}\nФамилия: #{@surname}\nОтчество: #{@patronymic}\nEmail: #{@email}\nGit: #{@git}\nТелефон: #{@phone}\nTelegram: #{@telegram}\n----------"
