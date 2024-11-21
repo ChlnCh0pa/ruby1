67480d1c90922205b59157defae5dfc39d51ce0d
Sergey Dudo <zamirdruazbu@gmail.com>
2024-11-21 19:20:13 +0300

    Lab2_Task1.7. Add phone number validation and improve constructor

--- student3/student.rb
+++ student3/student.rb
@@ -1,23 +1,33 @@
 class Student
-attr_accessor :name, :surname, :patronymic, :git, :email, :id, :phone, :tlegram
-def initialize (name:, surname:, patronymic:, git:, email:, id: nil, phone: nil, telegram: nil)
-@name = name
-@surname = surname
-@patronymic = patronymic
-@git = git
-@email = email
-@id = id 
-@phone = phone 
-@telegram = telegram
-end
+  attr_accessor :name, :surname, :patronymic, :git, :email, :id, :phone, :telegram
 
+  PHONE_REGEX = /^\+\d{11}$/
 
-
- 
-
-def to_s
-"Студент\nID: #{@id}\nИмя: #{@name}\nФамилия: #{@surname}\nОтчество: #{@patronymic}\nEmail: #{@email}\nGit: #{@git}\nТелефон: #{@phone}\nTelegram: #{@telegram}\n----------"
+  def self.valid_phone?(phone)
+    phone.match?(PHONE_REGEX)
   end
 
- 
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @surname = attributes[:surname]
+    @patronymic = attributes[:patronymic]
+    @git = attributes[:git]
+    @telegram = attributes[:telegram]
+    @id = attributes[:id]
+    @phone = attributes[:phone]
+    @email = attributes[:email]
+    validate_phone 
+  end
+
+  def to_s
+    "Студент\nID: #{@id}\nИмя: #{@name}\nФамилия: #{@surname}\nОтчество: #{@patronymic}\nEmail: #{@email}\nGit: #{@git}\nТелефон: #{@phone}\nTelegram: #{@telegram}\n----------"
+  end
+
+  private
+  def validate_phone
+    if @phone && !self.class.valid_phone?(@phone)
+      raise ArgumentError, "Некорректный номер телефона"
+    end
+  end
+  
 end
