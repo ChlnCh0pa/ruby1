26ac5062a11da0d10503278f49279ebada4106e2
Sergey Dudo <zamirdruazbu@gmail.com>
2024-11-21 20:16:44 +0300

    Lab2_Task1.8. Add validations for correct string format in other fields

--- student3/student.rb
+++ student3/student.rb
@@ -1,11 +1,42 @@
 class Student
-  attr_accessor :name, :surname, :patronymic, :git, :email, :id, :phone, :telegram
+  attr_accessor :name, :surname, :patronymic, :git, :email, :phone, :telegram
+  attr_reader :id
 
   PHONE_REGEX = /^\+\d{11}$/
+  NAME_REGEX = /^[A-Za-zА-Яа-я]+$/
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  GIT_REGEX = /^https:\/\/github\.com\/\w+$/
+  TELEGRAM_REGEX = /^@\w+$/
 
   def self.valid_phone?(phone)
     phone.match?(PHONE_REGEX)
   end
+
+  def self.valid_name?(name)
+    name.match?(NAME_REGEX)
+  end
+
+  def self.valid_email?(email)
+    email.match?(EMAIL_REGEX)
+  end
+
+  def self.valid_git?(git)
+    git.match?(GIT_REGEX)
+  end
+
+  def self.valid_telegram?(telegram)
+    telegram.match?(TELEGRAM_REGEX)
+  end
+
+  FIELD_VALIDATORS = {
+    name: :valid_name?,
+    surname: :valid_name?,
+    patronymic: :valid_name?,
+    email: :valid_email?,
+    git: :valid_git?,
+    phone: :valid_phone?,
+    telegram: :valid_telegram?
+  }
 
   def initialize(attributes = {})
     @name = attributes[:name]
@@ -16,7 +47,7 @@ class Student
     @id = attributes[:id]
     @phone = attributes[:phone]
     @email = attributes[:email]
-    validate_phone 
+    validate_fields
   end
 
   def to_s
@@ -24,10 +55,16 @@ class Student
   end
 
   private
-  def validate_phone
-    if @phone && !self.class.valid_phone?(@phone)
-      raise ArgumentError, "Некорректный номер телефона"
+  def validate_fields
+    FIELD_VALIDATORS.each do |field, method|
+      validate_field(field, method)
     end
   end
-  
+
+  def validate_field(field, validation_method)
+    value = instance_variable_get("@#{field}")
+    if value && !self.class.send(validation_method, value)
+      raise ArgumentError, "Invalid #{field}: #{value}"
+    end
+  end
 end
