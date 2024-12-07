b95137059d45d851dbfcddcc5ed19fa74a469725
Sergey Dudo <zamirdruazbu@gmail.com>
2024-12-07 07:46:48 +0300

    Update main.rb

--- student3/main.rb
+++ student3/main.rb
@@ -23,8 +23,19 @@ student2 = Student.new(
   telegram: '@sumkin'
 )
 
+student3 = Student.new(
+  id: '10',
+  name: 'Baralgin',
+  surname:'Grebneshchikov',
+  patronymic: 'Dimedrolovich',
+  git: 'github.com/grebenb',
+  telegram: '@agropromlox'
+  )
+
+student_from_base_student = StudentShortInfo.new_from_base_student(student3)
 student_short_info = "Agronom A.D.; github.com/elessar; agronome@rivendel.com, +79991234567, @deunedain_agropmrop"
 student_short_from_info = StudentShortInfo.new_from_string(id: 3, str: student_short_info)
+student4 = StudentShortInfo.new_from_base_student(student3)
 
 puts student1.to_s
 puts student2.to_s
@@ -37,7 +48,9 @@ student1.validate_git_and_contact
 
 puts student2.get_info
 
-puts " \nКраткая инфа:\n#{student_short_from_info.to_s}\n\n"
+puts "\n Краткая инфа:\n #{student_short_from_info.to_s},\n #{student_from_base_student}  \n\n"
+
+
 
 # Вывод гитa, инициалов и контактов через отдельные методы объекта класса
 puts student1.short_name
