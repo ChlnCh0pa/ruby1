698938e2f73d266dd31097e8920d07bc7ab39955
Sergey Dudo <zamirdruazbu@gmail.com>
2024-12-16 22:04:38 +0300

    Lab3_Task1.19. cyclic shift of array elements one position to the right

--- blocks_and_collections/blocks.rb
+++ blocks_and_collections/blocks.rb
@@ -1,24 +1,35 @@
 def cyclic_shift(array)
-array[-2..-1] + array[0...-2]
+  array[-2..-1] + array[0...-2]
 end
+
+def cyclic_shift_one(array)
+  array[-1..-1] + array[0...-1]
+end
+
 
 loop do
-puts "Выберите задачу для выполнения:"
-puts "1. Циклический сдвиг массива вправо на две позиции"
-puts "0. Выход"
+  puts "Выберите задачу для выполнения:"
+  puts "1. Циклический сдвиг массива вправо на две позиции"
+  puts "2. Циклический сдвиг массива вправо на одну позицию"
+  puts "0. Выход"
 
-choice = gets.to_i
+  choice = gets.to_i
 
-case choice
-when 1
-puts "Введите элементы массива через пробел:"
-input_array = gets.split.map(&:to_i)
-shifted_array = cyclic_shift(input_array)
-puts "Результат сдвига: #{shifted_array}"
-when 0
-puts "Выход из программы."
-break
-else
-puts "Неверный выбор. Попробуйте снова."
-end
+  case choice
+  when 1
+    puts "Введите элементы массива через пробел:"
+    input_array = gets.split.map(&:to_i)
+    shifted_array = cyclic_shift(input_array)
+    puts "Результат сдвига: #{shifted_array}"
+  when 2
+    puts "Введите элементы массива через пробел:"
+    input_array = gets.split.map(&:to_i)
+    shifted_array = cyclic_shift_one(input_array)
+    puts "Результат сдвига: #{shifted_array}"
+  when 0
+    puts "Выход из программы."
+    break
+  else
+    puts "Неверный выбор. Попробуйте снова."
+  end
 end
