c4e4c93770050d7e41c9dbd80224f285e623a1da
Sergey Dudo <zamirdruazbu@gmail.com>
2024-12-16 22:14:56 +0300

    Lab3_Task 1.55.  New ordered list by the number of occurrences of an element from the initial one

--- blocks_and_collections/blocks.rb
+++ blocks_and_collections/blocks.rb
@@ -1,4 +1,5 @@
-def count_min_elements(array)
-  min_value = array.min
-  array.count(min_value)
+def sort_by_frequency(array)
+  array.group_by { |e| e }              
+       .sort_by { |_, v| -v.size }
+       .flat_map { |k, v| [k] * v.size } 
 end
