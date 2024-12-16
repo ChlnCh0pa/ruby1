bb4c47bfe5524bf8e6222f238deab7f5eaf8be46
Sergey Dudo <zamirdruazbu@gmail.com>
2024-12-16 14:39:44 +0300

    2 - Corrected the output of short student information.

--- student3/main.rb
+++ student3/main.rb
@@ -4,7 +4,7 @@ require_relative 'student_short'
 
 student1 = Student.new(
   id: '1',
-  name: 'Leaman',
+  name: 'Leaman', 
   surname: 'Russ',
   patronymic: 'Slonovich',
   git: 'github.com/prospero',
@@ -26,16 +26,16 @@ student2 = Student.new(
 student3 = Student.new(
   id: '10',
   name: 'Baralgin',
-  surname:'Grebneshchikov',
   patronymic: 'Dimedrolovich',
+  surname:'Grebneshchikov',
   git: 'github.com/grebenb',
   telegram: '@agropromlox'
   )
 
-student_from_base_student = StudentShortInfo.new_from_base_student(student3)
+student_from_student = StudentShortInfo.new_from_student(student3)
 student_short_info = "Agronom A.D.; github.com/elessar; agronome@rivendel.com, +79991234567, @deunedain_agropmrop"
 student_short_from_info = StudentShortInfo.new_from_string(id: 3, str: student_short_info)
-student4 = StudentShortInfo.new_from_base_student(student3)
+student4 = StudentShortInfo.new_from_student(student3)
 
 puts student1.to_s
 puts student2.to_s
@@ -48,11 +48,10 @@ student1.validate_git_and_contact
 
 puts student2.get_info
 
-puts "\n Краткая инфа:\n #{student_short_from_info.to_s},\n #{student_from_base_student}  \n\n"
+puts "\n Краткая инфа:\n #{student_short_from_info.to_s},\n #{student_from_student}  \n\n"
 
 
 
-# Вывод гитa, инициалов и контактов через отдельные методы объекта класса
 puts student1.short_name
 puts student1.short_info
-puts student1.contact_info
+puts student1.contact
