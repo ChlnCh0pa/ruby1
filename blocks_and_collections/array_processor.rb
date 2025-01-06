a552d5c637342584445c84e378324a226c3bb001
Sergey Dudo <zamirdruazbu@gmail.com>
2025-01-06 05:59:39 +0300

    Lab3_Task2(ALL)

--- blocks_and_collections/array_processor.rb
+++ blocks_and_collections/array_processor.rb
@@ -4,12 +4,74 @@ class ArrayProcessor
   def initialize(array)
     @array = array.dup.freeze
   end
-  
-def select
+
+  def drop_while
+    return to_enum(:drop_while) unless block_given?
+    result = []
+    dropping = true
+
+    @array.each do |element|
+      if dropping && !yield(element)
+        dropping = false
+      end
+      result << element unless dropping
+    end
+
+    result
+  end
+
+  def max
+    return nil if @array.empty?
+    max_element = @array.first
+
+    @array.each do |element|
+      max_element = element if yield(max_element, element) < 0
+    end
+
+    max_element
+  end
+
+  def sort
+    return to_enum(:sort) unless block_given?
+    result = @array.dup
+
+    (result.length - 1).times do
+      (0...result.length - 1).each do |i|
+        if yield(result[i], result[i + 1]) > 0
+          result[i], result[i + 1] = result[i + 1], result[i]
+        end
+      end
+    end
+
+    result
+  end
+
+  def map
+    return to_enum(:map) unless block_given?
+    result = []
+
+    @array.each do |element|
+      result << yield(element)
+    end
+
+    result
+  end
+
+  def detect
+    return to_enum(:detect) unless block_given?
+
+    @array.each do |element|
+      return element if yield(element)
+    end
+
+    nil
+  end
+
+  def select
     return enum_for(:select) unless block_given?
 
     result = []
-    array.each do |element|
+    @array.each do |element|
       result << element if yield(element)
     end
     result
