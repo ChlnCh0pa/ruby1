61627d8c87b220eb86a54421b5d2f1d6e726c897
Sergey Dudo <zamirdruazbu@gmail.com>
2025-01-06 05:52:08 +0300

    Lab3_Task2.4, map

--- blocks_and_collections/array_processor.rb
+++ blocks_and_collections/array_processor.rb
@@ -5,22 +5,15 @@ class ArrayProcessor
     @array = array.dup.freeze
   end
 
-
-
-  def sort
-    return to_enum(:sort) unless block_given?
-    result = @array.dup
-
-    (result.length - 1).times do
-      (0...result.length - 1).each do |i|
-        if yield(result[i], result[i + 1]) > 0
-          result[i], result[i + 1] = result[i + 1], result[i]
-        end
-      end
+  def map
+    return to_enum(:map) unless block_given?
+    result = []
+    @array.each do |element|
+      result << yield(element)
     end
 
     result
   end
 
- 
+
 end
