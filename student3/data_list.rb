01fe55ab2d0a766cfa56bf34a005b989d27c58d1
Sergey Dudo <zamirdruazbu@gmail.com>
2025-01-23 06:47:41 +0300

    Lab4_Task7. Implemented DataListStudentShort class with template method pattern

--- student3/data_list.rb
+++ student3/data_list.rb
@@ -1,6 +1,6 @@
 class DataList
   def initialize(data)
-    @data = data.sort_by(&:id) 
+    @data = data.sort_by(&:id)  
     @selected = []
   end
 
@@ -20,14 +20,27 @@ class DataList
 
 
   def get_names
-    raise NotImplementedError, "This method should be implemented in a subclass"
+    ['ID'] + specific_names
   end
 
+
   def get_data
-    raise NotImplementedError, "This method should be implemented in a subclass"
+    data_with_indices = @data.each_with_index.map do |item, index|
+      [index + 1] + specific_data(item)
+    end
+    DataTable.new(data_with_indices)
   end
 
   private
+
+
+  def specific_names
+    raise NotImplementedError, "Этот метод должен быть реализован в подклассе"
+  end
+
+  def specific_data(item)
+    raise NotImplementedError, "Этот метод должен быть реализован в подклассе"
+  end
 
   attr_reader :data
 end
--- student3/data_list_student_short.rb
+++ student3/data_list_student_short.rb
@@ -1,16 +1,12 @@
 require_relative 'data_list'
 require_relative 'data_table'
-class DataListStudentShort < DataList
-  def get_names
-    ['Surname and initials', 'GitHub', 'Contacts']
-  end
 
-  def get_data
-    data_with_indices = @data.each_with_index.map do |student, index|
-      [index + 1, student.surname_initials, student.git, student.contact]
-    end
-    DataTable.new(data_with_indices)
+class DataListStudentShort < DataList
+  private
+  def specific_names
+    ['Фамилия и инициалы', 'GitHub', 'Контакты']
+  end
+  def specific_data(item)
+    [item.surname_initials, item.git, item.contact]
   end
 end
-
-
