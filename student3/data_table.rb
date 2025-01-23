011fadd7fde281242536bc5656f18b8ec1209eb5
Sergey Dudo <zamirdruazbu@gmail.com>
2025-01-23 06:19:49 +0300

    Lab4_Task1.2. Added methods to get element, row count, and column count in DataTable

--- student3/data_table.rb
+++ student3/data_table.rb
@@ -1,11 +1,23 @@
 class DataTable
+
   def initialize(data)
     @data = data
   end
 
-  def display_data
-    @data.each do |row|
-      puts row.join(", ")
-    end
+  def get_element(row, column)
+    @data[row][column]
+  end
+
+
+  def row_count
+    @data.length
+  end
+
+
+  def column_count
+    @data[0].length
+  end
+
+ 
   end
 end
