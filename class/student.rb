8a4f6fccfa816832225508fad3e7102077bcb6bb
Sergey Dudo <zamirdruazbu@gmail.com>
2025-01-24 11:48:30 +0300

    Lab4_Task3.7. Refactor Student class constructor to accept hash parameters

--- class/student.rb
+++ class/student.rb
@@ -1,22 +1,28 @@
 require_relative 'base_student'
+require 'date'
+
 class Student < BaseStudent
   attr_reader :phone, :telegram, :email, :birth_date
 
-  def initialize(id: nil, surname:, name:, patronymic:, git: nil, phone: nil, telegram: nil, email: nil, birth_date: nil)
-    set_contacts(phone: phone, telegram: telegram, email: email)
-    self.birth_date = birth_date if birth_date
-    super(surname: surname, name: name, patronymic: patronymic, git: git, id: id, contact: contact)
+
+  def initialize(attributes = {})
+    set_contacts(phone: attributes[:phone], telegram: attributes[:telegram], email: attributes[:email])
+    self.birth_date = attributes[:birth_date] if attributes[:birth_date]
+    super(surname: attributes[:surname], name: attributes[:name], patronymic: attributes[:patronymic], git: attributes[:git], id: attributes[:id], contact: contact)
   end
+
 
   def self.birth_date_valid?(date)
     return true if date.is_a?(Date)
     Date.parse(date.to_s) rescue false
   end
 
+  # Установка даты рождения с валидацией
   def birth_date=(date)
     raise ArgumentError, 'Некорректная дата рождения' unless self.class.birth_date_valid?(date)
     @birth_date = date.is_a?(Date) ? date : Date.parse(date)
   end
+
 
   def set_contacts(phone: nil, telegram: nil, email: nil)
     self.phone = phone if phone
@@ -25,21 +31,26 @@ class Student < BaseStudent
     self.contact = "#{self.phone || self.telegram || self.email || nil}"
   end
 
+
   def self.phone_valid?(phone)
     phone.to_s.match?(/^(\d{11}|\+\d{11})$/)
   end
+
 
   def self.telegram_valid?(telegram)
     telegram.match?(/^@[\w]+$/)
   end
 
+
   def self.email_valid?(email)
     email.match?(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
   end
 
+
   def get_info
     "Инициалы: #{surname_initials}; Git: #{self.git};  Контакты: #{self.contact}"
   end
+
 
   def to_s
     details = []
@@ -50,9 +61,23 @@ class Student < BaseStudent
     details.join("\n")
   end
 
-  
+
+  def to_h
+    {
+      id: @id,
+      surname: @surname,
+      name: @name,
+      patronymic: @patronymic,
+      git: @git,
+      phone: @phone,
+      telegram: @telegram,
+      email: @email,
+      birth_date: @birth_date.to_s
+    }
+  end
 
   private
+
 
   def phone=(phone)
     if phone.nil? || Student.phone_valid?(phone)
@@ -69,6 +94,7 @@ class Student < BaseStudent
       raise ArgumentError, "Неверный Telegram: #{id} #{surname} #{name}"
     end
   end
+
 
   def email=(email)
     if email.nil? || Student.email_valid?(email)
