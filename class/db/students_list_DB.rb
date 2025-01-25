1c6ff27e40ac790d703c350d6d306dac8c88a00c
Sergey Dudo <zamirdruazbu@gmail.com>
2025-01-25 05:06:45 +0300

    Lab4. Used  singleton to avoid creating identical students.

--- class/db/students_list_DB.rb
+++ class/db/students_list_DB.rb
@@ -4,16 +4,35 @@ require_relative 'C:/Users/Sergey/AppData/Local/GitHubDesktop/ruby/class/Data_li
 require_relative 'C:/Users/Sergey/AppData/Local/GitHubDesktop/ruby/class/base_student.rb'
 require_relative 'C:/Users/Sergey/AppData/Local/GitHubDesktop/ruby/class/Data_table.rb'
 require_relative 'C:/Users/Sergey/AppData/Local/GitHubDesktop/ruby/class/strategy_list_file.rb'
+require 'json'
+require 'yaml'
 
 class DatabaseHandler
+  
+  @@instance = nil
+
   def initialize(db_params)
     @conn = PG.connect(db_params)
+    @students_cache = {}
+  end
+
+  # Метод для получения единственного экземпляра
+  def self.instance(db_params = nil)
+    @@instance ||= new(db_params)
   end
 
   def close
     @conn.close
   end
 
+  def read_students
+    file_strategy.read(file_path)
+  end
+
+  def write_students(students)
+    file_strategy.write(file_path, students)
+  end
+  
   def get_student_by_id(id)
     result = @conn.exec_params('SELECT * FROM student WHERE id = $1', [id])
     if result.ntuples > 0
@@ -56,6 +75,10 @@ class DatabaseHandler
   end
 
   def add_student(new_student)
+    if student_exists?(new_student)
+      raise "Студент с такими данными уже существует!"
+    end
+
     result = @conn.exec_params(
       'INSERT INTO student (surname, name, patronymic, git, phone, email, telegram) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
       [
@@ -68,6 +91,10 @@ class DatabaseHandler
         new_student.telegram
       ]
     )
+    
+    # Кэшируем студента для быстрого поиска по ранее добавленным данным
+    @students_cache[new_student.git] = new_student
+    
     result[0]['id'].to_i
   end
 
@@ -94,5 +121,20 @@ class DatabaseHandler
   def get_student_count
     result = @conn.exec('SELECT COUNT(*) FROM student')
     result[0]['count'].to_i
+  end
+  
+  private
+  
+
+  def student_exists?(new_student)
+    @students_cache.key?(new_student.git) || existing_student_in_db?(new_student)
+  end
+
+  def existing_student_in_db?(new_student)
+    result = @conn.exec_params(
+      'SELECT COUNT(*) FROM student WHERE git = $1 OR email = $2',
+      [new_student.git, new_student.email]
+    )
+    result[0]['count'].to_i > 0
   end
 end
